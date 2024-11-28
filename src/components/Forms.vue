286be78c8234a91a6dbc70f7e433cd3ddaba6b76
root <dennis.schmidt2221@gmail.com>
2024-11-28 23:33:27 +0100

    Form Styling

--- src/components/Forms.vue
+++ src/components/Forms.vue
@@ -4,15 +4,22 @@
 
 <template>
   <div class="form-container">
-    <h2>Logo:</h2>
-    <div class="btn-group" role="group" aria-label="Basic outlined example">
-      <button type="button" class="btn btn-outline-primary">Farbig</button>
-      <button type="button" class="btn btn-outline-primary">Transparent</button>
+    <div class="styling">
+      <h2>Styling</h2>
+      <div class="btn-group" role="group" aria-label="Basic outlined example">
+        <button type="button" class="btn btn-outline-primary">Farbig</button>
+        <button type="button" class="btn btn-outline-primary">Transparent</button>
+      </div>
+      <label for="customRange2" class="form-label">Transperenz:</label>
+      <input type="range" class="form-range" min="0" max="100" id="customRange2">
     </div>
-    <div class="input-group">
-      <div class="input-group flex-nowrap">
-        <span class="input-group-text" id="addon-wrapping">@</span>
-        <input type="text" class="form-control" placeholder="Unterschrift (Vor- und Nachname" aria-label="Username" aria-describedby="addon-wrapping">
+    <div class="forms">
+      <h2>Informationen</h2>
+      <div class="input-group">
+        <div class="input-group flex-nowrap">
+          <span class="input-group-text" id="addon-wrapping">@</span>
+          <input type="text" class="form-control" placeholder="Unterschrift (Vor- und Nachname" aria-label="Username" aria-describedby="addon-wrapping">
+        </div>
       </div>
     </div>
   </div>
@@ -22,12 +29,33 @@
 
 .form-container {
   display: flex;
-  flex-direction: column;
+  flex-direction: row-reverse;
   gap: 1vh;
+  //background-color: red;
+  justify-content: start;
 }
 
-.input-group-text, .form-control {
-  border-color: black;
+.forms {
+  display: flex;
+  flex-direction: column;
+  //background-color: #323232;
+  width: 100%;
+}
+
+.styling {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.input-group-text, .form-control, .btn-outline-primary {
+  border-color: #ffffff;
+  background-color: #323232;
+  color: white;
+}
+
+.form-control {
+
 }
 
 </style>
